e28cbe6ba7ada72779081cfba596718086717b11
spyros <spyros@cs.vu.nl>
2013-06-24 10:36:58 +0200

    Added a default Transport for each operation mode.

--- src/peernet/transport/Transport.java
+++ src/peernet/transport/Transport.java
@@ -4,6 +4,10 @@
  */
 package peernet.transport;
 
+import java.util.Properties;
+
+import peernet.config.Configuration;
+import peernet.core.Engine;
 import peernet.core.Node;
 
 
@@ -14,10 +18,52 @@ import peernet.core.Node;
  * This is a generic transport interface. It is used to send messages to other
  * nodes.
  * 
+ * Typically, a single Transport class is used by all protocols, but it is
+ * possible for individual protocols to define separate Transport classes.
+ * 
+ * Note that separate Transport instance(s) are created for each Node.
+ * 
  * @author Spyros Voulgaris
  */
 public abstract class Transport implements Cloneable
 {
+  /**
+   * Parameter name in configuration that attaches a transport layer protocol to
+   * a protocol.
+   * 
+   * @config
+   */
+  private static final String PAR_TRANSPORT = "transport";
+
+  private static final String defaultTransportSim = "peernet.transport.UniformRandomTransport";
+  private static final String defaultTransportEmu = "peernet.transport.UniformRandomTransport";
+  private static final String defaultTransportNet = "peernet.transport.TransportUDP";
+  private static Transport defaultTransportInstance = null;
+
+
+
+
+
+  public static Properties setDefaultTransports()
+  {
+    Properties p = new Properties();
+
+    if (!Configuration.contains("sim.transport"))
+      p.setProperty("sim.transport", defaultTransportSim);
+
+    if (!Configuration.contains("emu.transport"))
+      p.setProperty("emu.transport", defaultTransportEmu);
+
+    if (!Configuration.contains("net.transport"))
+      p.setProperty("net.transport", defaultTransportNet);
+    
+    return p;
+  }
+
+
+
+
+
   /**
    * Used to send a message to another node, given the node's address.
    */
@@ -37,5 +83,29 @@ public abstract class Transport implements Cloneable
       e.printStackTrace();
     }
     return transport;
+  }
+
+
+
+  public static Transport defaultTransportInstance()
+  {
+    if (defaultTransportInstance == null)
+    {
+      String key = Engine.getType().toString().toLowerCase() + "." + PAR_TRANSPORT;
+      if (Configuration.contains(key))
+        defaultTransportInstance = (Transport) Configuration.getInstance(key);
+      else // no default transport defined ==> use the absolutely default one
+        switch (Engine.getType())
+        {
+          case SIM:
+          case EMU:
+            //XXX defaultTransportInstance = (Transport) Configuration.getInstance(defaultTransportSim);
+          case NET:
+            //defaultTransportInstance = (Transport) Configuration.getInstance(defaultTransportNet);
+          default:
+            break;
+        }
+    }
+    return defaultTransportInstance;
   }
 }
